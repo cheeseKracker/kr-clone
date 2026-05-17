75f108c0ca6bc30d5f74df8f6d02af162d6b388e
Bithotra Karak <karak.bitihotra@gmail.com>
2026-05-17 23:35:56 +0530

    cleared blogs

--- src/components/exif-blog-nav.tsx
+++ src/components/exif-blog-nav.tsx
@@ -5,12 +5,13 @@ import Link from "next/link";
 export default function ExifBlogNav() {
   return (
     <section className="exif-nav">
-      <p className="exif-nav-title">Exif: Blog</p>
+      <p className="exif-nav-title">XML: Blog</p>
       <nav className="exif-nav-links" aria-label="Exif Blog Links">
         <Link href="/">Root</Link>
         <Link href="/blog/minfo">Is This Normal?</Link>
-        <Link href="/blog/dog-years">Bookshelf</Link>
         <Link href="/sandbox">Old Projects</Link>
+        <Link href="/blog">Blog</Link>
+        <Link href="/blog/dog-years">Bookshelf</Link>
         <Link href="/blog/uses">Gear</Link>
         <a href="https://www.yourworldoftext.com/~keyaar/" target="_blank" rel="noreferrer noopener">
           Say Hi!
